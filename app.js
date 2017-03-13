a466713f9b1bab73f3f749a1e5402355a42e5253
jicjjang <jicjjang12@gmail.com>
2017-03-14 00:51:59 +0900

    Change some log and check hook term.

--- app.js
+++ app.js
@@ -49,14 +49,47 @@ function getDataTimer () {
       console.log('current time : ' + currentTime)
       const res = JSON.parse(response.body)
       for (const data in res) {
-        console.log('setting time[ ' + count++ + ' ] : ' + res[data].hookTime)
-        if (currentTime === res[data].hookTime) {
+        console.log(`setting time[ ${count++} ] : ${res[data].hookTime}, hook term: ${(Number(res[data].hookTerm === 0) ? 'no term' : res[data].hookTerm)}`)
+        if (currentTime === res[data].hookTime || checkHookTerm(currentTime, res[data].hookTime, res[data].hookTerm)) {
           sendMessage(res[data].id, res[data].name, res[data].image, res[data].data)
         }
       }
       console.log('\n=========================\n')
     }
   })
+}
+
+function checkHookTerm (currentTime, hookTime, hookTerm) {
+  const term0 = Number(hookTime.split(':')[1])
+  const term1 = Number(hookTime.split(':')[1]) % 10
+  const term2 = (Number(hookTime.split(':')[1]) + 5) % 10
+  const term3 = (Number(hookTime.split(':')[1]) + 15) % 60
+  const term4 = (Number(hookTime.split(':')[1]) + 30) % 60
+  const term5 = (Number(hookTime.split(':')[1]) + 45) % 60
+
+  if (Number(hookTerm) === 5) {
+    if (Number(currentTime.split(':')[1]) % 10 === term1 || Number(currentTime.split(':')[1]) % 10 === term2) {
+      return true
+    }
+  } else if (Number(hookTerm) === 10) {
+    if (Number(currentTime.split(':')[1]) % 10 === term1) {
+      return true
+    }
+  } else if (Number(hookTerm) === 15) {
+    if (Number(currentTime.split(':')[1]) === term0 || Number(currentTime.split(':')[1]) === term3 ||
+        Number(currentTime.split(':')[1]) === term4 || Number(currentTime.split(':')[1]) === term5) {
+      return true
+    }
+  } else if (Number(hookTerm) === 30) {
+    if (Number(currentTime.split(':')[1]) === term0 || Number(currentTime.split(':')[1]) === term4) {
+      return true
+    }
+  } else if (Number(hookTerm) === 60) {
+    if (Number(currentTime.split(':')[1]) === term0) {
+      return true
+    }
+  }
+  return false
 }
 
 function checkHookType (req, res, next) {
